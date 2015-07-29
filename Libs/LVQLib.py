8e6bcbed78da4456c8c8faedb8a4affbe74f12f5
David Siah <dsiah@villanova.edu>
2015-07-29 09:18:57 -0700

    libs updated

--- Libs/LVQLib.py
+++ Libs/LVQLib.py
@@ -1,29 +1,43 @@
 
 # coding: utf-8
 
-# In[285]:
+# In[128]:
 
 import os, csv
+from random import shuffle
 
 
-# In[286]:
+# In[186]:
 
 class LVQData:
     def __init__(self):
         """
         LVQData Constructor:
-            LVQData is a list of input vectors paired with their correct instrument classification.
+            LVQData is a list of input vectors paired with their correct 
+            instrument classification.
             LVQNet will iterate over this structure until it converges.
         """
+        
         self.data          = [] # list of tuples 
         self.instrumentMap = {} # map integers with instruments (labels)
         self.instrumentNum = 0  # current integer instrument (to neuron)
+        
+    def shuffleData(self, n):
+        """
+        Shuffle the training data n times (in place)
+            - n must be a valid integer
+        """
+        
+        for i in range(n):
+            shuffle(self.data)
+        return self.data
     
     def getVectorData(self, index):
         """
         Get the STFT of the Frame (1025 bins) at the specified index
             index must be an valid integer
         """
+        
         return self.data[index][0]
     
     def getVectorLabel(self, index):
@@ -31,6 +45,7 @@ class LVQData:
         Get the label of the Frame at the specified index
             - index must be an valid integer
         """
+        
         return self.data[index][1]
     
     def getVector(self, index):
@@ -38,6 +53,7 @@ class LVQData:
         Get the STFT data and label of the Frame at the specified index
             - index must be an valid integer
         """
+        
         return self.data[index][0], self.data[index][1]
     
     def lookupInstrument(self, index):
@@ -45,14 +61,31 @@ class LVQData:
         Lookup the instument specified by the number of the neuron
             - index must be a valid integer
         """
+        
         return self.instrumentMap[index]
-    
+
+    def inverseLookupInstrument(self, name):
+        """
+        Lookup the instrument by name. The neuron number will be returned
+            - name must be a valid string for one of the instruments
+            
+        (TODO) Use the third method here for small speedups later on.
+        http://stackoverflow.com/questions/8023306/get-key-by-value-in-dictionary
+        """
+        
+        for key, instrument in self.instrumentMap.items():
+            if (instrument == name):
+                return key
+        
     def loadCSV(self, filepath, label):
         """
         Add a STFT datapoint and label to the LVQData list
-            - filepath must be valid string from current working directory (os.getcwd() to check)
-            - label must be a string describing the instrument of the csv datapoints for entire CSV
+            - filepath must be valid string from current working directory 
+            (os.getcwd() to check)
+            - label must be a string describing the instrument of the csv 
+            datapoints for entire CSV
         """
+        
         with open(filepath, 'r') as f:
             read = csv.reader(f, delimiter=',')
             read.next()
@@ -60,14 +93,15 @@ class LVQData:
             for row in read:
                 data_struct = (arrayParser(row[1]), label) # Tuple with STFT bin list and then the label
                 self.data.append(data_struct)
-        
-            self.instrumentMap[self.instrumentNum] = label
-            self.instrumentNum += 1
+            
+            if not(label in self.instrumentMap.items()):
+                self.instrumentMap[self.instrumentNum] = label
+                self.instrumentNum += 1
             
         return self.data
 
 
-# In[287]:
+# In[187]:
 
 class LVQNeuron:
     def __init__(self, name):
@@ -76,9 +110,11 @@ class LVQNeuron:
     
     def setWeights(self, weights):
         """
-        Import a vector of information (the first entry) for initializing next unique neuron
+        Import a vector of information (the first entry) for 
+        initializing next unique neuron.
             - weights is a string from the import of a CSV
         """
+        
         weights = weights.replace('[', '').replace(']', '').split(',')
         
         for weight in weights:
@@ -89,18 +125,17 @@ class LVQNeuron:
         return len(self.weights)
 
 
-# In[288]:
+# In[188]:
 
 class LVQNet:
     def __init__(self, inCount, outCount):
         self.inputs   = inCount
         self.outputs  = outCount
-        self.alpha    = 0.1
+        self.alpha    = 0.005
         self.csvCount = 0  # Limit the CSVs input to number of output neurons
         self.iter     = 0  # Number of times the vectors have been used in training
         self.neurons  = {} # Numbered index map (to outputs)
-
-              
+        
         for n in range(outCount):
             curr_neuron = LVQNeuron(n)
             self.neurons[n] = curr_neuron
@@ -113,25 +148,43 @@ class LVQNet:
         Get the current weights of the neuron specified
             - neuronNo must be a valid integer
         """
+        
         return self.neurons[neuronNo].weights
     
     def setWeights(self, neuronNo, newWeights):
         """
         Change the neuron weights with new Weight vector
             - neuronNo must be a valid integer
-            - newWeights must be a list of numbers the same length as the previous weights
+            - newWeights must be a list of numbers the same length 
+            as the previous weights.
         """
+        
         prev = self.neurons[neuronNo]
         
         if len(prev) == len(newWeights):
             prev.weights = newWeights
+    
+    def saveLVQNet(self, filepath):
+        """
+        Incomplete! Do not use!
+        """
+        with open(filepath, 'wb') as f:
+            cursor = csv.writer(f, delimiter=',')
+            cursor.writerow([self.inputs, self.outputs, self.alpha, 
+                             self.csvCount, self.iter])
+            
+            for key, val in self.neurons.items():
+                cursor.writerow([key, val])
         
     # STEP 0
     def enterCSV(self, filepath): 
         """
-        Initialize neurons with first row of data in CSV (each CSV should represent a neuron)
-            - filepath must be valid string from current working directory (os.getcwd() to check)
+        Initialize neurons with first row of data in CSV 
+        (each CSV should represent a neuron).
+            - filepath must be valid string from current working
+            directory (os.getcwd() to check)
         """
+        
         if self.csvCount >= self.outputs:
             print "Reached limit of neurons"
             return
@@ -149,10 +202,13 @@ class LVQNet:
     # STEP 3.1
     def edist(self, inputs, weights):
         """
-        Helper function to get the Euclidean distance between input vector and neuron weights
-            - inputs must be list of numbers with same number of items as weights
+        Helper function to get the Euclidean distance between input 
+        vector and neuron weights.
+            - inputs must be list of numbers with same number 
+            of items as weights
             - weights must be a valid list of neuron weights 
         """
+        
         euclideanDistance = 0
         
         if len(inputs) != len(weights):
@@ -169,9 +225,11 @@ class LVQNet:
     # STEP 3.2 
     def minDist(self, inputVector):
         """
-        Given a input vector calculate the closest (guess) neuron to classify as
-            - inputVector must be list of numbers with same number of items as the neuron weights
+        Given an input vector, calculate the closest (guess) neuron to classify
+            - inputVector must be list of numbers with the same number of items 
+            as the neuron weights
         """
+        
         scores = [] # Euclidean Distances
         
         for neuron in self.neurons:
@@ -181,14 +239,36 @@ class LVQNet:
         minNeuronIndex = scores.index(min(scores))
         return minNeuronIndex
     
+    def minDistGradient(self, inputVector):
+        """
+        Given an input vector, calculate the list of neurons in order
+            - inputVector must be a list of numbers with the same number of 
+            items as the neuron weights
+        """
+        
+        scores = {} # Euclidean Distances
+        iterator = 0
+        
+        for neuron in self.neurons:
+            wunit = self.neurons[neuron].weights
+            scores[iterator] = self.edist(inputVector, wunit)
+            iterator += 1
+
+        return scores
+    
     # STEP 4.1
     def calibrate(self, neuronNo, guessNo):
         """
-        Given a neuron number and a guess neuron number, first check to see if the guess was correct 
-        or not and then calibrate the neurons accordingly (increase if correct, decrease if incorrect)
-            - neuronNo must be a valid integer (between 0 and total number of neurons - 1)
-            - guessNo  must be a valid integer (between 0 and total number of neurons - 1)
+        Given a neuron number and a guess neuron number, 
+        first check to see if the guess was correct 
+        or not and then calibrate the neurons accordingly 
+        (increase if correct, decrease if incorrect).
+            - neuronNo must be a valid integer 
+            (between 0 and total number of neurons - 1)
+            - guessNo  must be a valid integer 
+            (between 0 and total number of neurons - 1)
         """
+        
         neuron      = dataset.getVectorLabel(neuronNo) 
         inputVector = dataset.getVectorData(neuronNo)
         guess       = dataset.lookupInstrument(guessNo) 
@@ -198,21 +278,22 @@ class LVQNet:
         subfunc = lambda oldWeight, vec: oldWeight - self.alpha * (vec - oldWeight)
 
         if neuron == guess: 
-            newWeights = map(addfunc, weights, inputVector) # assign weights as new weights
+            newWeights = map(addfunc, weights, inputVector) 
             self.setWeights(guessNo, newWeights)
             return newWeights
         else:
-            newWeights = map(subfunc, weights, inputVector) # assign weights as new weights
+            newWeights = map(subfunc, weights, inputVector) 
             self.setWeights(guessNo, newWeights)
             return newWeights
 
     # STEP 5
-    def reduceAlpha(self, value):
+    def reduceAlpha(self, beta):
         """
         Helper function to reduce alpha value
-            - value must be a number greater than 0 but less than / equal to alpha
+            - beta must be valid float between 1 and 100 (percent decrease)
         """
-        self.alpha -= value
+        
+        self.alpha = self.alpha * (beta / 100.00)
         return self.alpha
     
     def iteration(self, dataset): # Helper function for iterating through during run function
@@ -222,6 +303,7 @@ class LVQNet:
             - dataset must be a list of tuples including vectors with correct dimensions 
             and classification labels
         """
+        
         length = range(len(dataset.data))
         for v in length:
             # v is the dataset.data index while vector is the input vector
@@ -229,14 +311,21 @@ class LVQNet:
             guess = self.minDist(vector[0])
             newWeights = self.calibrate(v, guess)
             
+    
+    def guess(self, stft):
+        return self.minDist(stft)
+            
     @staticmethod        
     def meanSquaredError(oldList, newList):
         """
-        MeanSquaredError will return a averages from the difference between the old neuron weight
-        and new neuron weight squared.
-            - oldList must be a list of weights that has the same dimensions as newList
-            - newList must be a list of weights that has the same dimensions as the neuron weights
+        MeanSquaredError will return a averages from the difference 
+        between the old neuron weight and new neuron weight squared.
+            - oldList must be a list of weights that has the same 
+            dimensions as newList
+            - newList must be a list of weights that has the same 
+            dimensions as the neuron weights
         """
+        
         diffSquared = lambda a, b : (a - b) ** 2
         average     = lambda arr  :  float(sum(arr) / len(arr))
         
@@ -245,10 +334,12 @@ class LVQNet:
         
     def run(self, ds):
         """
-        Run the algorithm given a LVQData object that has been initialized with all the CSVs
+        Run the algorithm given a LVQData object that has been 
+        initialized with all the CSVs.
             - ds must have same number of CSVs initialized as neurons training 
             (or classifications)
         """
+        
         if (len(ds.data) == 0):
             print "Need to initialize data in order to run the neural net."
             return
@@ -262,7 +353,6 @@ class LVQNet:
             oldW.append(w)
             
         self.iteration(ds)
-        
 
         newW = [] # Debug After
         for neuron in range(len(self.neurons)):
@@ -277,12 +367,12 @@ class LVQNet:
         return average(means)
 
 
-# In[289]:
+# In[189]:
 
 def arrayParser(arr):
     """
-    ArrayParser takes an array created from reading a CSV into memory and turns the lists-strings
-    and casts them to a valid python list
+    ArrayParser takes an array created from reading a CSV into memory and 
+    turns the lists-strings and casts them to a valid python list
         - arr must be a string that has the same format as a python list
     """
     # CSV usage: cast string list to python list
@@ -291,7 +381,7 @@ def arrayParser(arr):
     return smooth_stage_2
 
 
-# In[292]:
+# In[199]:
 
 ### Driver: Outline of the API / Algorithm in use    
 if __name__ == '__main__':
@@ -300,16 +390,48 @@ if __name__ == '__main__':
     
     # Enter data (1-1 CSV to Output Neurons) 
     # Initializes the neurons with first onset from each unique CSV
-    koho.enterCSV('./data/snareFrames.csv')
-    koho.enterCSV('./data/kickDrumFrames.csv')
+    koho.enterCSV('./Data/snareFrames.csv')
+    koho.enterCSV('./Data/kickDrumFrames.csv')
     
     # Instantiate LVQ Training Data Structure and load rest of CSVs with labels
     dataset = LVQData()
-    dataset.loadCSV('./data/snareFrames.csv',    'snare')
-    dataset.loadCSV('./data/kickDrumFrames.csv', 'kick-drum')
+    dataset.loadCSV('./Data/snareFrames.csv',    'snare-drum')    # 0
+    dataset.loadCSV('./Data/kickDrumFrames.csv', 'kick-drum')     # 1
 
-    sigma = 1
-    while (sigma > 0.0000000001):
-        sigma = koho.run(dataset)
+    sigma = 1 # Sigma tracks the meanSquaredError between each calibration iteration in the LVQ training
+    while (koho.iter < 2500):
+        dataset.shuffleData(3)
+        sigma  = koho.run(dataset)
+        alphie = koho.reduceAlpha(99)
         print sigma
+
+
+# In[200]:
+
+if __name__ == '__main__':
+    testset = LVQData()
+    
+    testset.loadCSV('./Data/Test-Onsets/kickDrumTestFrames.csv', 'kick-drum')
+    testset.loadCSV('./Data/kickDrumFrames.csv', 'kick-drum')
+    testset.loadCSV('./Data/Test-Onsets/snareTestFrames.csv',    'snare-drum')
+    
+    # Test Script
+    print len(testset.data), "samples in testset. Commencing trials:"
+    print "Categories:", dataset.instrumentMap
+    
+    for frame in testset.data:
+        guess         = koho.guess(frame[0])
+        guessGradient = koho.minDistGradient(frame[0])
+        correctNum    = dataset.inverseLookupInstrument(frame[1])
+        correctName   = frame[1]
+        
+        # Debug testing the neurons
+        if (dataset.lookupInstrument(guess) != frame[1]):
+            print "Incorrect Guess : %s (%d)"  % (dataset.lookupInstrument(guess), guess),
+            print "[correct answer is: %s (%d)]" % (correctName, correctNum)
+            print guessGradient
+            print "Off by %f" % (abs(guessGradient[correctNum] - guessGradient[guess]))
+        else:
+            print "Correct Guess: %s (%d)" % (dataset.lookupInstrument(guess), guess)
+            print guessGradient
 
