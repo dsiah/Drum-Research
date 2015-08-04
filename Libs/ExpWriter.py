f0dc80341ac31973b1b273ccbb923918bce08c36
David Siah <dsiah@villanova.edu>
2015-08-04 15:03:51 -0700

    writer is now functional

--- Libs/ExpWriter.py
+++ Libs/ExpWriter.py
@@ -7,26 +7,26 @@ class SEWriter():
         # (TODO) add path logic
         ext = '.sexp'
         self.document = open(path + name + ext, 'w')
-        self.document.write("(network (name %s)\n" % name)
+        self.document.write("(network (name %s)" % name)
         
     def writeSExp(self, operator, *args):
         f = self.document
-        f.write("(" + operator)
+        f.write("\n\t(%s"   % str(operator))
         
         for arg in args:
             f.write(" %s" % str(arg))
         
-        f.write(")\n")
+        f.write(")")
         
     def writeSexpArr(self, operator, arr):
         f = self.document
-        f.write("(" + operator)
+        f.write("\n\t(%s"   % str(operator))
         
         for unit in arr:
-            f.write(unit + " ")
+            f.write(" %s" % str(unit))
         
-        f.write(")\n")
+        f.write(")")
     
     def finish(self):
-        self.document.write("\n)")
+        self.document.write(")")
         self.document.close()
